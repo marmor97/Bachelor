29b96db6c99c194d200bd166c630140eb79d1dc3
AstridSlet <astrid.rybner@hotmail.com>
2020-10-13 13:35:40 +0200

    Main code

--- functions/Inspection.R
+++ functions/Inspection.R
@@ -1,30 +1,29 @@
 #Investigating data
 library(pacman)
 p_load(tidyverse, groupdata2, dplyr)
-setwd("~/Uni/Bachelor/Data")
 
-demodata <- read_csv("DemoData.csv")
+#demodata <- read_csv("DemoData.csv")
 
 #Number of participants 
 
-demodata  %>% group_by( Gender) %>% summarise(n = n())
+#demodata  %>% group_by( Gender) %>% summarise(n = n())
 
 #Partitioning function
 
-partition_func <- function(dataframe, hold_size = 0.2, seed){
+partition_func <- function(dataframe, hold_size = 0.2, seed=2020, n=158){
   set.seed(seed)
-  test_females = dataframe %>% filter(Gender == "Female")  %>%  sample_n(round(nrow(dataframe)*hold_size,0)/2)
+  test_females = dataframe %>% filter(Gender == "Female")  %>%  sample_n(round(n*hold_size,0)/2)
   train_females = dataframe[dataframe$Gender == "Female" & !dataframe$ID %in% test_females$ID,]
-  test_males = dataframe %>% filter(Gender == "Male")  %>%  sample_n(round(nrow(dataframe)*hold_size,0)/2)
+  test_males = dataframe %>% filter(Gender == "Male")  %>%  sample_n(round(n*hold_size,0)/2)
   train_males = dataframe[dataframe$Gender == "Male" & !dataframe$ID %in% test_males$ID,]
   holdout = rbind(test_females, test_males)
   train_all = rbind(train_females, train_males)
   return(list(holdout, train_all))
 }
 
-splittet_data <- partition_func(demodata, seed = 30)
-hold_out <- splittet_data[[1]]
-train <- splittet_data[[2]]
+#splittet_data <- partition_func(demodata, seed = 30)
+#hold_out <- splittet_data[[1]]
+#train <- splittet_data[[2]]
 
 
 
