4dcb48eecdd7f9687770800c0f7d407d186c0432
marmor97 <mariemortensen97@gmail.com>
2020-10-28 14:09:11 +0100

    Changes in loop - now with a write_csv that saves the selected lasso coefficients for each fold

--- functions/Lasso loop function.R
+++ functions/Lasso loop function.R
@@ -39,12 +39,16 @@ for(i in (1:length(unique(fold_train$.folds)))){
            test_fold = paste(i)) %>% 
     filter(abs > 0)
   
-  name <- paste("lasso_feature_fold",i, sep = "_")
-  write.csv(lasso_coef, paste(name, "csv", sep = "."))
+  # name <- paste("lasso_feature_testfold",i, sep = "_")
+  # write.csv(lasso_coef, paste(name, "csv", sep = "."))
+  # test_csv <- test[,c("ID", "Gender", "Diagnosis", 
+  #                           colnames(test[,(colnames(test) %in% lasso_coef$term)]))]
+  new_fold_train <- fold_train[,c("ID", "Gender", "Diagnosis", colnames(fold_train[,(colnames(fold_train) %in% lasso_coef$term)]))]
+  name_2 <- paste("test_fold",i, sep = "_")
+  write.csv(new_fold_train, paste(name_2, "csv", sep = "."))
   
-  test_csv <- test[,c("ID", "Gender", "Diagnosis", 
-                            colnames(test[,(colnames(test) %in% lasso_coef$term)]))]
-  
-  write.csv(test_csv, paste(i, "test_set.csv", sep = "_"))
+  # write.csv(test_csv, paste(i, "test_set.csv", sep = "_"))
 }
 }
+
+lasso_function(train_scaled, folds = 5, id_col = "ID")
