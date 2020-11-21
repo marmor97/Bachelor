f68d9808b2e2b31029e337586852efc82456888a
stinenyhus <stnyl@hotmail.com>
2020-11-21 16:02:50 +0100

    Plotting function updated, reading files smarter way

--- functions/Plotting.R
+++ functions/Plotting.R
@@ -2,30 +2,36 @@ library(pacman)
 p_load(ggplot2, tidyverse, wesanderson)
 
 ####Function that prepares data for plotting####
-for_plotting <- function(data_list, kernel){ #It takes a list of dataframes - one for each fold in the SVMs
-  for (i in 1:length(data_list)){ #For all elements in the list
-    colnames(data_list[[i]])[6] <- "weighted_avg" #Change column name to better one
-    colnames(data_list[[i]])[5] <- "macro_avg"
-    colnames(data_list[[i]])[1] <- "metric"
-    data_list[[i]][,7] <- paste(i) #Make column with fold number
-    colnames(data_list[[i]])[7] <- "Fold" #Change that column name to fold
-    data_list[[i]][,8] <- kernel #Make column name with kernel type
-    colnames(data_list[[i]])[8] <- "Kernel" #Change that column name
-    if (i ==1){
-      data <- as.data.frame(data_list[[1]]) #If the first element: initiate dataframe
-    }
-    if (i != 1){ #All other elements: add to dataframe
-      data <- data %>% rbind(data_list[[i]])
-    }
-  }
-  return(data)
-}
+
+
+prepare_file <- function(filename, path = getwd()) {
+  filepath = paste0(path, "/class_reports/", filename, sep = "")
+  file = read_csv(filepath)
+  colnames(file)[1] <- "Metric"
+  colnames(file)[2] <- "TD"
+  colnames(file)[3] <- "ASD"
+  colnames(file)[4] <- "Accuracy"
+  colnames(file)[5] <- "Macro_avg"
+  colnames(file)[6] <- "Weighted_avg"
+  file$Fold         <- rep((str_extract(filename,"(\\d)")),4)
+  file$Featureset   <- rep(paste0(regmatches(filename, gregexpr("[a-z]*", filename))[[1]][1]),4)
+                      if ((regmatches(filename, 
+                                      gregexpr("[a-z]*", 
+                                      filename))[[1]][5])=="rbf"|(regmatches(filename, 
+                                                                         gregexpr("[a-z]*", 
+                                                                         filename))[[1]][5]) == "sigmoid"){
+  file$Kernel       <- rep(paste0(regmatches(filename,gregexpr("[a-z]*",filename))[[1]][5]),4) } else {
+  file$Kernel       <- rep("linear",4)
+                     }
+                    
+  return(file)
+}  
 
 
 plot_performance <- function(data, title){
   data %>% 
-    subset(metric == "f1-score") %>% 
-    ggplot(aes(Kernel, weighted_avg, fill = Fold))+
+    subset(Metric == "f1-score") %>% 
+    ggplot(aes(Kernel, Weighted_avg, fill = Fold))+
     geom_bar(stat = "summary", position = "dodge")+
     labs(x = "Kernel",y = "Performance", title = title)+
     scale_fill_manual(values= wes_palette("Rushmore1", n = 5))+
