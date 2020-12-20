c4b8e7b0e8d0443c1742e811696c6809539eab5f
stinenyhus <stnyl@hotmail.com>
2020-12-20 18:02:44 +0100

    performance plot updated so it looks better

--- functions/Plotting.R
+++ functions/Plotting.R
@@ -25,14 +25,19 @@ prepare_file <- function(filename, path = getwd()) {
   colnames(file)[6] <- "Weighted_avg"
   file$Fold         <- rep((str_extract(filename,"(\\d)")),4)
   file$Featureset   <- rep(paste0(regmatches(filename, gregexpr("[a-z]*", filename))[[1]][1]),4)
-                      if ((regmatches(filename, 
-                                      gregexpr("[a-z]*", 
-                                      filename))[[1]][5])=="rbf"|(regmatches(filename, 
-                                                                         gregexpr("[a-z]*", 
-                                                                         filename))[[1]][5]) == "sigmoid"){
-  file$Kernel       <- rep(paste0(regmatches(filename,gregexpr("[a-z]*",filename))[[1]][5]),4) } else {
-  file$Kernel       <- rep("linear",4)
-                     }
+  
+
+                      if ((regmatches(filename, gregexpr("[a-z]*", filename))[[1]][5]) == "sigmoid"){
+  file$Kernel       <- rep("Sigmoid", 4)
+                      }
+  
+                      else if ((regmatches(filename, gregexpr("[a-z]*", filename))[[1]][5]) == "rbf"){
+  file$Kernel       <- rep("RBF", 4)
+                      }
+  
+                      else {
+  file$Kernel       <- rep("Linear",4)
+                      }
                     
   return(file)
 }  
@@ -43,8 +48,9 @@ plot_performance <- function(data, title){
     subset(Metric == "f1-score") %>% 
     ggplot(aes(Kernel, Macro_avg, fill = Fold))+
     geom_bar(stat = "summary", position = "dodge")+
-    labs(x = "Kernel",y = "F1-score", title = title)+
-    scale_fill_manual(values= wes_palette("Rushmore1", n = 5))+
+    labs(x = "Kernel",y = "F-score", title = title, fill = "Model")+
+    scale_fill_brewer(palette = "Accent") + #Accent is good
+  #  scale_fill_manual(values= wes_palette("Darjeeling2", n = 5))+
     theme_light()+
     theme(text=element_text(family="serif", size = 20))
 }
@@ -58,13 +64,13 @@ pre_recall <- function(dataframe, featureset, kernel, title){
     melt(id.vars = c("Metric", "Featureset", "Kernel", "Fold"), variable.name = "Diagnosis")
   for (i in 1:length(unique(dataframe$Fold))){
     print(i)
-    plot = data %>% 
+    plot = data %>%
       ggplot(aes(Metric,value, fill = Diagnosis))+
       geom_bar(fun.data = mean_se, stat = "summary", position = "dodge", size = 0.1)+
       labs(title = title, y = "Value")+
       scale_fill_manual(values= wes_palette("Royal1", n = 2))+
       theme_light()+
-      theme(text=element_text(family="serif", size = 20))+
+      theme(text=element_text(family="serif", size = 15))+
       facet_wrap(~Fold, nrow =1, ncol = 5)
   }
   return(plot)
